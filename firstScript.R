cee8f5294516106618b79f4df0fc42b052efd9fc
theMLtrader <Luca.Parlamento.1@gmail.com>
2019-02-22 11:04:29 +1100

    plot

--- firstScript.R
+++ firstScript.R
@@ -1,2 +1,18 @@
-getwd()
-read.csv("data/SAFI_clean.csv")
+#getwd()
+#read.csv("data/SAFI_clean.csv")
+#x = sqrt(53216)
+
+library(tidyverse)
+
+interviews_plotting <- read_csv("data_output/interviews_plotting.csv")
+
+summary(interviews_plotting)
+
+sort(names(interviews_plotting))
+
+set.seed(1)
+
+ggplot(data = interviews_plotting, 
+       aes(x = no_membrs, y =number_items))+
+      geom_jitter(alpha = 0.2)
+
